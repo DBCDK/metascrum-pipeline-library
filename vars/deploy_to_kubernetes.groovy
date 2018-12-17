56165a178a7050593a3a7a90977c5982db603ccc
Jens Stein Jørgensen <jsj@dbc.dk>
2018-12-17 08:29:26 +0100

    Wait for roll-out to finish

--- vars/deploy_to_kubernetes.groovy
+++ vars/deploy_to_kubernetes.groovy
@@ -7,6 +7,8 @@ def call(String deployment_path, String kubecert, String namespace,
 		steps) {
 	steps.sh """#!/usr/bin/env bash
 		set -xe
+		DEPLOYMENT_NAME=\$(kubectl get -n ${namespace} -f ${deployment_path} -o name | grep deployment)
 		kubectl --kubeconfig ${kubecert} apply -f ${deployment_path} -n ${namespace}
+		kubectl rollout status deployment -n ${namespace} \$DEPLOYMENT_NAME
 	"""
 }
