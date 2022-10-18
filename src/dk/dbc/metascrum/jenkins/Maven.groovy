e483668b69359c2f78a230f911cbe85f699a35c4
Jens Grønborg Andersen <jega@dbc.dk>
2022-10-18 17:36:43 +0200

    testing jenkins shared libs

--- src/dk/dbc/metascrum/jenkins/Maven.groovy
+++ src/dk/dbc/metascrum/jenkins/Maven.groovy
@@ -1,12 +1,12 @@
 package dk.dbc.metascrum.jenkins
 
-class MetascrumMaven implements Serializable {
+class Maven implements Serializable {
     static def verify(step) {
         echo "Building and stuff..."
-        step.sh "mvn -D sourcepath=src/main/java verify pmd:pmd javadoc:aggregate"
+//        step.sh "mvn -D sourcepath=src/main/java verify pmd:pmd javadoc:aggregate"
 //        step.junit testResults: '**/target/*-reports/TEST-*.xml'
 //        step.script {
-//            def java = scanForIssues tool: [$class: 'Java']
+//            def ja va = scanForIssues tool: [$class: 'Java']
 //            def javadoc = scanForIssues tool: [$class: 'JavaDoc']
 //            publishIssues issues: [java, javadoc]
 //
