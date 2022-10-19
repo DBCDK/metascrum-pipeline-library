f56bd760aa1e44ee8d7291123a9019e731403516
Jens Grønborg Andersen <jega@dbc.dk>
2022-10-19 10:50:02 +0200

    testing jenkins shared libs

--- src/dk/dbc/metascrum/jenkins/Maven.groovy
+++ src/dk/dbc/metascrum/jenkins/Maven.groovy
@@ -1,15 +1,21 @@
 package dk.dbc.metascrum.jenkins
 
 class Maven implements Serializable {
-    static def verify(script) {
-        script.echo "Building and stuff..."
-        script.sh "mvn -D sourcepath=src/main/java verify pmd:pmd javadoc:aggregate"
+    static def verify(script, pmdEnabled = true) {
+        script.echo script
+        script.sh "mvn -B -D sourcepath=src/main/java verify pmd:pmd javadoc:aggregate"
         script.junit testResults: '**/target/*-reports/TEST-*.xml'
-        def java = script.scanForIssues tool: [$class: 'Java']
-        def javadoc = script.scanForIssues tool: [$class: 'JavaDoc']
-        script.publishIssues issues: [java, javadoc]
-        def pmd = script.scanForIssues tool: [$class: 'Pmd'], pattern: '**/target/pmd.xml'
-        script.publishIssues issues: [pmd]
+        if (pmdEnabled) {
+            def java = script.scanForIssues tool: [$class: 'Java']
+            def javadoc = script.scanForIssues tool: [$class: 'JavaDoc']
+            script.publishIssues issues: [java, javadoc]
+            def pmd = script.scanForIssues tool: [$class: 'Pmd'], pattern: '**/target/pmd.xml'
+            script.publishIssues issues: [pmd]
+        }
+    }
+
+    static def deploy(script) {
+        script.sh "mvn -D deploy -Dmaven.test.skip=true"
     }
 }
 
