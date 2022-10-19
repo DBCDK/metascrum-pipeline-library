1d0b13b5f1e030f31f9a033295009e414f7aa2bc
Jens Grønborg Andersen <jega@dbc.dk>
2022-10-19 11:48:46 +0200

    testing jenkins shared libs

--- src/dk/dbc/metascrum/jenkins/Maven.groovy
+++ src/dk/dbc/metascrum/jenkins/Maven.groovy
@@ -2,7 +2,7 @@ package dk.dbc.metascrum.jenkins
 
 class Maven implements Serializable {
     static def verify(script, pmdEnabled = true) {
-        script.sh "mvn -B -Dmaven.repo.local=\\$WORKSPACE/.repo verify pmd:pmd javadoc:aggregate"
+        script.sh "mvn -B -Dmaven.repo.local=\$WORKSPACE/.repo verify pmd:pmd javadoc:aggregate"
         script.junit testResults: '**/target/*-reports/TEST-*.xml'
         if (pmdEnabled) {
             def java = script.scanForIssues tool: [$class: 'Java']
