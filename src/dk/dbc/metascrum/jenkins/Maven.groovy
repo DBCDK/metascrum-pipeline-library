331e5003ba435bb1012ec6f123ed5d69b06bb837
Jens Grønborg Andersen <jega@dbc.dk>
2022-11-09 12:23:11 +0100

    Added profiles and projectList

--- src/dk/dbc/metascrum/jenkins/Maven.groovy
+++ src/dk/dbc/metascrum/jenkins/Maven.groovy
@@ -1,7 +1,11 @@
 package dk.dbc.metascrum.jenkins
 
 class Maven implements Serializable {
-    static def verify(script, pmdEnabled = true) {
+    static def verify(script, pmdEnabled = true, profiles="") {
+        def prof = ""
+        if(!profiles.isEmpty()) {
+            prof = "-P\"${profiles}\""
+        }
         script.sh "mvn -B -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn verify pmd:pmd javadoc:aggregate"
         script.junit testResults: '**/target/*-reports/TEST-*.xml'
         if (pmdEnabled) {
@@ -13,8 +17,12 @@ class Maven implements Serializable {
         }
     }
 
-    static def deploy(script) {
-        script.sh "mvn deploy -Dmaven.test.skip=true"
+    static def deploy(script, projectList = "") {
+        def pl = ""
+        if (!projectList.isEmpty()) {
+            pl = "-pl ${projectList} -am"
+        }
+        script.sh "mvn deploy -Dmaven.test.skip=true ${pl}"
     }
 }
 
